4be343ecbcb4cd44e185cc62c1db64b16b48a73b
Rumman Waqar <rumman.waqar05@gmail.com>
2017-05-04 12:55:10 +0500

    cleaned up drive interface

---
+++ scripts/drive_interface.py
@@ -0,0 +1,149 @@
+"""
+Google drive interface for arvp deep-learning data processing
+"""
+
+import os
+import httplib2
+import io
+import argparse
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from googleapiclient.http import MediaIoBaseDownload
+
+# readonly scope for GDrive
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+# directory stores saved authenticated credentials
+CREDENTIAL_DIR = os.path.join(os.path.expanduser('~'), '.credentials')
+# application name
+APPLICATION_NAME = 'Arvp Deep-learning Dataset'
+# folder IDs for various folders
+FOLDER_IDS = {
+    'root': '0BxfNkdUtSbXRSFp1cFVncWpiNFU',             # deep-learning
+    'datasets': '0BxfNkdUtSbXRWDI3cmk0SzJXWW8',         # deep-learning/datasets
+    'tagged_videos': '0BxfNkdUtSbXRcVpIZ0lZRzI5X3c',    # deep-learning/tagged_videos
+    'raw_videos': '0BxfNkdUtSbXRU2M3dTY1NEJvbFk',       # deep-learning/raw_videos
+    'image_db': '1AhC41ZzZrlNcrpuljxzoGBLm289_R1zZntM1yDei0Uk' # deep-learning/Image Database
+}
+
+
+class DriveInterface(object):
+    """
+    Creates a drive connection, and fetches various files
+    """
+    def __init__(self, secrets_file='./client_secret.json'):
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+        credentials = DriveInterface.get_credentials(secrets_file=secrets_file, flags=flags)
+        http = credentials.authorize(httplib2.Http())
+        self.drive_service = discovery.build('drive', 'v3', http=http)
+
+    def get_list_of_videos(self, read_info=False):
+        """
+        gets list of videos (and their info) inside raw_videos
+        :param read_info: flag to read video info
+        :return: 
+        """
+        query = "'{0}' in parents and mimeType contains 'video'".format(FOLDER_IDS['raw_videos'])
+        results = self.drive_service.files().list(q=query).execute()
+        items = results.get('files', [])
+        if not items:
+            print('No files found in folder. Ensure that the folder ID is correct and it still exists.')
+            return None
+        print('Files:')
+        video_files = {}
+        for item in items:
+            video_files[item['name']] = {
+                'id': item['id']
+            }
+            print(item['name'])
+            if read_info:
+                info = self.get_video_info(item['name'])
+                video_files[item['name']].update(info)
+        return video_files
+
+    def get_video_info(self, video_name):
+        """
+        gets info for video file in raw_videos by reading the text file associated with video
+        :param video_name: name of video file
+        :return: dictionary with video information
+        """
+        query = "'{0}' in parents and name = '{1}.txt' and mimeType contains 'text'".format(
+            FOLDER_IDS['raw_videos'], os.path.splitext(video_name)[0]
+        )
+        results = self.drive_service.files().list(q=query).execute()
+        results = results.get('files', [])
+        if len(results) != 1:
+            print('Error retrieving txt file for {0}'.format(video_name))
+            return None
+        print("Reading file: {0}".format(results[0]['name']))
+        info = {}
+        for line in self.read_text_file(results[0]['id']).strip().split('\n'):
+            key, value = line.split(':')
+            info[key.strip()] = value.strip()
+        return info
+
+    def get_folder_id_by_name(self, folder_name):
+        """
+        get google drive id for specified folder
+        :param folder_name: name of folder
+        :return: google drive folder id
+        """
+        query = "name = '{0}' and mimeType = '{1}'".format(
+            folder_name, 'application/vnd.google-apps.folder'
+        )
+        results = self.drive_service.files().list(q=query).execute()
+        if len(results['files']) == 0:
+            print('No folders with name {0} found'.format(folder_name))
+            return None
+        elif len(results['files']) > 1:
+            print('{0} folder with name {1} found'.format(len(results['files']), folder_name))
+            for folder in results['files']:
+                print('{0}: {1}'.format(folder['name'], folder['id']))
+            return None
+        else:
+            return results['files'][0]['id']
+
+    def read_text_file(self, file_id, progress=False):
+        """
+        reads a text file from drive to memory
+        :param file_id: google drive file id
+        :param progress: flag to show progress print statements
+        :return: string contents of file
+        """
+        request = self.drive_service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            if progress:
+                print("Downloaded {0}%".format(int(status.progress() * 100)))
+        contents = fh.getvalue().decode("utf-8")
+        return contents
+
+    @staticmethod
+    def get_credentials(secrets_file, flags):
+        """
+        Gets valid user credentials from storage.
+        If nothing has been stored, or if the stored credentials are incorrect, uses flow to obtain new auth credentials
+    
+        retrieved credentials are stored in CREDENTIAL_DIR/arvp-dl-dataset.json
+        :param secrets_file: file containing client secrets
+        :param flags: command line flags from argparser. Needed for run_flow function 
+        :return: authenticated credentials
+        """
+        # setup path for credentials file
+        if not os.path.exists(CREDENTIAL_DIR):
+            os.makedirs(CREDENTIAL_DIR)
+        credential_path = os.path.join(CREDENTIAL_DIR, 'arvp-dl-dataset.json')
+
+        # get credentials from stored file or use flow to retrieve new credentials
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(secrets_file, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            credentials = tools.run_flow(flow, store, flags)
+            print('Storing credentials to ' + credential_path)
+        return credentials
---
+++ scripts/get_videos_list.py
@@ -0,0 +1,45 @@
+"""
+Gets info for all videos in raw_videos folder
+Usage: ./get_videos_list <output_csv>
+    if output csv file given, then output goes to csv instead of std::out
+"""
+
+import sys
+from drive_interface import DriveInterface
+
+
+def main():
+    output_file = None
+    if len(sys.argv) == 2:
+        output_file = sys.argv[1]
+
+    drive = DriveInterface()
+    videos = drive.get_list_of_videos(read_info=True)
+
+    if output_file is None:
+        # output to screen
+        for video, info in videos.items():
+            print('{}:'.format(video))
+            print('\tdate: {}'.format(info['Date']))
+            print('\tcamera: {}'.format(info['Camera']))
+            print('\tpool: {}'.format(info['Pool']))
+            print('\torientation: {}'.format(info['Orientation']))
+            print('\tcontent: {}'.format(info['Content']))
+            print('\tconditions: {}'.format(info['Conditions']))
+            print('---------')
+    else:
+        # output to csv
+        print('Writing video info to {}'.format(output_file))
+        with open(output_file, 'w') as ofile:
+            ofile.write('Filename;Date;Camera;Pool;Orientation;Content;Conditions\n')
+            for video, info in videos.items():
+                ofile.write(video + ';')
+                ofile.write(info['Date'] + ';')
+                ofile.write(info['Camera'] + ';')
+                ofile.write(info['Pool'] + ';')
+                ofile.write(info['Orientation'] + ';')
+                ofile.write(info['Content'] + ';')
+                ofile.write(info['Conditions'] + '\n')
+
+if __name__ == '__main__':
+    main()
